a3c3d1635cfb86124b17549e2bcb82c65fe132ef
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-10-02 11:21:24 +0300

    v0

--- scripts/dds_checks.sql
+++ scripts/dds_checks.sql
@@ -20,18 +20,18 @@ INSERT INTO dds.quality_of_service_checks
 			 FROM stage.mdaudit_questions
 			 WHERE period = '{{execution_date.replace(day=1)}}')
 	SELECT
-		replace(data -> 'id'), '"', '')::INT 							AS id
-		,replace(data -> 'template_id'), '"', '')::INT 					AS question_i
-		,replace(data -> 'shop_id'), '"', '')::INT						AS shop_id
-		,replace(data -> 'division_id'), '"', '')::INT  				AS division_id
-		,replace(data -> 'resolver_id'), '"', '')::INT  				AS resolver_id
-		,replace(data -> 'resolve_date'), '"', '')::DATE 				AS resolve_date
-		,replace(data -> 'start_time'), '"', '')::TIMESTAMP 			AS start_time
-		,replace(data -> 'finish_time'), '"', '')::TIMESTAMP 			AS finish_time
-		,replace(data -> 'last_modified_at'), '"', '')::TIMESTAMP 		AS last_modified_at
-		,replace(data -> 'grade'), '"', '')::NUMERIC(6,3) 				AS grade
-		,replace(data -> 'comment'), '"', '')::VARCHAR(1000)			AS comment
-		,replace(data -> 'status'), '"', '')::VARCHAR(100)				AS status
+		replace(data -> 'id', '"', '')::INT 							AS id
+		,replace(data -> 'template_id', '"', '')::INT 					AS question_i
+		,replace(data -> 'shop_id', '"', '')::INT						AS shop_id
+		,replace(data -> 'division_id', '"', '')::INT  					AS division_id
+		,replace(data -> 'resolver_id', '"', '')::INT  					AS resolver_id
+		,replace(data -> 'resolve_date', '"', '')::DATE 				AS resolve_date
+		,replace(data -> 'start_time', '"', '')::TIMESTAMP 				AS start_time
+		,replace(data -> 'finish_time', '"', '')::TIMESTAMP 			AS finish_time
+		,replace(data -> 'last_modified_at', '"', '')::TIMESTAMP 		AS last_modified_at
+		,replace(data -> 'grade', '"', '')::NUMERIC(6,3) 				AS grade
+		,replace(data -> 'comment', '"', '')::VARCHAR(1000)				AS comment
+		,replace(data -> 'status', '"', '')::VARCHAR(100)				AS status
 		,period
 	FROM data;
 
