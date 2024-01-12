8663c68aafcc493005bfaad912fbc2e8c578f866
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2024-01-12 10:45:08 +0300

    new operator

--- scripts/dds_shops.sql
+++ scripts/dds_shops.sql
@@ -4,18 +4,12 @@ DELETE FROM dds.quality_of_service_shops
 WHERE id IN 
 	(
         SELECT DISTINCT
-	    replace((json_array_elements(data) ->> 'id'), '"', '')::int
+	    replace((data ->> 'id'), '"', '')::int
         FROM stage.mdaudit_shops
     );
 
 INSERT INTO dds.quality_of_service_shops
 (id, active, sap, locality, address, city, latitude, longitude, region_id)
-WITH 
-    data AS
-    (
-        SELECT json_array_elements(data) as data
-        FROM stage.mdaudit_shops
-    )         
 SELECT DISTINCT
 	replace((data ->> 'id'), '"', '')::int                  	AS id
 	,replace((data ->> 'active'), '"', '')::bool   				AS active
@@ -26,6 +20,6 @@ SELECT DISTINCT
 	,replace((data ->> 'latitude'), '"', '')::numeric(20,10)    AS latitude
 	,replace((data ->> 'longitude'), '"', '')::numeric(20,10)	AS longitude
     ,replace((data ->> 'regionId'), '"', '')::int               AS region_id
-FROM data AS d;
+FROM stage.mdaudit_shops;
 
 COMMIT TRANSACTION;
