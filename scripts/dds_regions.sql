61adf21667489ac0a750545adca38dfb41557453
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2024-01-12 10:19:57 +0300

    new operator

--- scripts/dds_regions.sql
+++ scripts/dds_regions.sql
@@ -5,7 +5,7 @@ WHERE id IN (SELECT DISTINCT
          	    replace(data ->> 'region_id', '"', '')::int
              FROM stage.mdaudit_checklists
              WHERE last_modified_at >= {{execution_date.date() - params.delta}}
-                AND last_modified_at < {{next_execution_date}});
+                AND last_modified_at < {{next_execution_date.date()}});
 
 INSERT INTO dds.quality_of_service_regions
 (
