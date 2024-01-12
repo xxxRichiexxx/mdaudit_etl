16783821bca38729ad64408100d8b9990b2272f4
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2024-01-12 11:34:08 +0300

    new operator

--- scripts/dds_resolvers.sql
+++ scripts/dds_resolvers.sql
@@ -4,9 +4,10 @@ DELETE FROM dds.quality_of_service_resolvers
 WHERE id IN 
 	(
         SELECT DISTINCT
-	    replace((json_array_elements(data) ->> 'resolver_id'), '"', '')::int
-        FROM stage.mdaudit_questions
-        WHERE period = '{{execution_date.replace(day=1)}}'
+	    replace((data ->> 'resolver_id'), '"', '')::int
+        FROM stage.mdaudit_checklists
+        WHERE last_modified_at >= '{{execution_date.date() - params.delta}}'
+            AND last_modified_at < '{{next_execution_date.date()}}'
     );
 
 INSERT INTO dds.quality_of_service_resolvers
@@ -14,18 +15,13 @@ INSERT INTO dds.quality_of_service_resolvers
     id
     ,resolver_first_name
     ,resolver_last_name
-)
-WITH 
-    data AS
-    (
-        SELECT json_array_elements(data) as data
-        FROM stage.mdaudit_questions
-        WHERE period = '{{execution_date.replace(day=1)}}'
-    )         
+)         
 SELECT DISTINCT
 	replace((data ->> 'resolver_id'), '"', '')::int						as id
 	,replace((data ->> 'resolver_first_name'), '"', '')::varchar(500)	as resolver_first_name
 	,replace((data ->> 'resolver_last_name'), '"', '')::varchar(500)	as resolver_last_name
-FROM data;
+FROM stage.mdaudit_checklists
+WHERE last_modified_at >= '{{execution_date.date() - params.delta}}'
+    AND last_modified_at < '{{next_execution_date.date()}}';
 
 COMMIT TRANSACTION;
